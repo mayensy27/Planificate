82d97d5de20a5f27a27d60c51a41b9c7d9716e9d
maygd <maygd27@gmail.com>
2021-03-10 17:36:00 +0100

    Empleo de la libreria McalendarView para el marcado de dias de las tomas

--- app/src/main/java/cat/urv/deim/asm/p2/planificate/ui/calendario/CalendarioActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/planificate/ui/calendario/CalendarioActivity.java
@@ -1,17 +1,20 @@
 package cat.urv.deim.asm.p2.planificate.ui.calendario;
 
 import android.os.Bundle;
-import android.widget.CalendarView;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import cat.urv.deim.asm.p2.planificate.R;
+import sun.bob.mcalendarview.MCalendarView;
+import sun.bob.mcalendarview.listeners.OnDateClickListener;
+import sun.bob.mcalendarview.vo.DateData;
 
 public class CalendarioActivity extends AppCompatActivity {
 
 //    TextView retorno_texto_calendario;
-CalendarView calendarView;
+ MCalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +37,20 @@ CalendarView calendarView;
         datePickerDialog.show();*/
 
     calendarView=findViewById(R.id.calendarView);
-    calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
-        String fecha= dayOfMonth +"/"+(month+1) +"/"+year;
-        Toast.makeText(CalendarioActivity.this,fecha,Toast.LENGTH_LONG).show();
+
+    ///MARCACION DE DIAS DEL MES
+        calendarView.markDate(2021, 3, 29);
+        calendarView.markDate(2021, 3, 27);
+        calendarView.markDate(2021, 4, 25);
+
+
+    calendarView.setOnDateClickListener(new OnDateClickListener() {
+        @Override
+        public void onDateClick(View view, DateData date) {
+            Toast.makeText(CalendarioActivity.this,"Hoy es: "+date.getDayString()+"/"+date.getMonth()+"/"+date.getYear(),Toast.LENGTH_LONG).show();
+        }
     });
+
+
     }
 }
