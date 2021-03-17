a2b18f005b8f9f205fac2a3708dcb20e58484a2b
maygd <maygd27@gmail.com>
2021-03-17 12:56:08 +0100

    Se quita la actividad CalendarioActivity y se introduce su codigo en el fragmento del Calendario

--- app/src/main/java/cat/urv/deim/asm/p2/planificate/SegudaPreguntaActivity.java
+++ app/src/main/java/cat/urv/deim/asm/p2/planificate/SegudaPreguntaActivity.java
@@ -100,7 +100,7 @@ public class SegudaPreguntaActivity extends AppCompatActivity {
 
             if(preferences.getBoolean("registrada", true)) {
 
-                Intent intent = new Intent(SegudaPreguntaActivity.this, MainActivity.class);
+                Intent intent = new Intent(SegudaPreguntaActivity.this, RegistroUsuariaActivity.class);
                 startActivity(intent);
             }else{
                 Intent intent = new Intent(SegudaPreguntaActivity.this, MainActivity.class);
