b8b062de65ee8975063ea3536943360553e31b3f
maygd <maygd27@gmail.com>
2021-03-19 12:59:38 +0100

    PESTAÑA CALENDARIO: -Cambio: se deja de utilizar el Calendario (y todos sus derivados) -Uso: Se realiza una ListView, donde se registran las tomas (fechas y estados) BLISTERS:  - Se incorpora la verrificacion de las tomas en un mismo dia (Evitando asi que se tomen dos o mas en un mismo dia)

--- app/src/main/java/cat/urv/deim/asm/p2/planificate/ui/blister21/Blister21Activity1.java
+++ app/src/main/java/cat/urv/deim/asm/p2/planificate/ui/blister21/Blister21Activity1.java
@@ -15,6 +15,7 @@ import java.util.Locale;
 
 import cat.urv.deim.asm.p2.planificate.Popup;
 import cat.urv.deim.asm.p2.planificate.R;
+import cat.urv.deim.asm.p2.planificate.SegudaPreguntaActivity;
 
 public class Blister21Activity1 extends AppCompatActivity {
     ImageButton cargarBlisterSiguiente;
@@ -36,27 +37,38 @@ public class Blister21Activity1 extends AppCompatActivity {
     public void cargarBlister(View view) {
         if(cargarBlisterSiguiente.isClickable()){
 
-            Intent x = new Intent(this, Blister21Activity2.class);
-            startActivity(x);
-            finish();
-            Intent i = new Intent(this, Popup.class);
-            startActivity(i);
-            finish();
-
-
-            // TOMA 2 DIA DE LA PILDRA
-
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-M-dd", Locale.getDefault()); //Cambiado para que concuerde con la fecha que me proporciona el Calendar
             Date date = new Date();
 
             String fecha = dateFormat.format(date);
-
             SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
-            SharedPreferences.Editor objEditor = preferences.edit();
-            objEditor.putString("tomaBlister_2", fecha); // dado que a partir de ahora no será la pirmera vez, lo ponemos false
-            objEditor.apply();
-            objEditor.putInt("dia_2", 2); // dado que a partir de ahora no será la pirmera vez, lo ponemos false
-            objEditor.apply();
+
+            Intent x = new Intent(this, Blister21Activity2.class);
+            startActivity(x);
+            finish();
+
+            //CONTROL DE LAS TOMAS EL MISMO DIA (PARA EVITAR LAS TOMAS EN UN MISMO DIA)
+            if(fecha.equals(preferences.getString("tomaBlister_1", ""))){
+                Intent i = new Intent(this, SegudaPreguntaActivity.class);
+                startActivity(i);
+                finish();
+            }
+            else{
+                Intent i = new Intent(this, Popup.class);
+                startActivity(i);
+                finish();
+                // TOMA 2 DIA DE LA PILDRA
+
+
+
+                SharedPreferences.Editor objEditor = preferences.edit();
+                objEditor.putString("tomaBlister_2", fecha); // dado que a partir de ahora no será la pirmera vez, lo ponemos false
+                objEditor.apply();
+                objEditor.putInt("dia_2", 2); // dado que a partir de ahora no será la pirmera vez, lo ponemos false
+                objEditor.apply();
+            }
+
+
 
         }
     }
--- app/src/main/java/cat/urv/deim/asm/p2/planificate/ui/calendario/CaledarioFragment.java
+++ app/src/main/java/cat/urv/deim/asm/p2/planificate/ui/calendario/CaledarioFragment.java
@@ -1,8 +1,6 @@
 package cat.urv.deim.asm.p2.planificate.ui.calendario;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +10,6 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import com.google.android.material.datepicker.MaterialDatePicker;
-
-import java.util.Calendar;
 
 import cat.urv.deim.asm.p2.planificate.R;
 
@@ -30,95 +24,14 @@ public class CaledarioFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Button abrir_calendario = view.findViewById(R.id.boton_calendario);
-
-        long today =MaterialDatePicker.todayInUtcMilliseconds();
-
-        MaterialDatePicker.Builder builder=MaterialDatePicker.Builder.datePicker();  //seleccionar
-        builder.setTitleText("SELECCIONA UNA FECHA");
-        final  MaterialDatePicker materialDatePicker=builder.build();
-
+        Button abrir_calendario= view.findViewById(R.id.boton_calendario);
         abrir_calendario.setOnClickListener(v -> {
+          Intent i= new Intent(getContext(),CalendarioActivity.class);
+            startActivity(i);
 
-        materialDatePicker.show(requireActivity().getSupportFragmentManager(),"DATE_PICKER");
 
         });
 
-        materialDatePicker.addOnPositiveButtonClickListener(selection -> {
-
-            Calendar calendario= Calendar.getInstance();
-            int ano=calendario.get(Calendar.YEAR);
-            int mes=calendario.get(Calendar.MONTH);
-            int dia=calendario.get(Calendar.DAY_OF_MONTH);
-            String fecha = ano + "-" + (mes + 1) + "-" + dia;
-
-            SharedPreferences preferences = requireActivity().getSharedPreferences("datos", Context.MODE_PRIVATE);
-
-
-            //TOMA PRIMER DIA DE LA PILDORA
-            if (fecha.equals(preferences.getString("tomaBlister_1", "")) && !preferences.getBoolean("primeravez_blister1", true) && preferences.getInt("dia_1", 0) == 1) {
-                //  retorno_texto_calendario.setText(preferences.getString("tomaBlister21_1", ""));
-                            /*retorno_texto_calendario.setText("Te has tomado la píldora: dia 1\n" + "El estado de ánimo" +
-                                    "ha sido: " + preferences.getInt("nivel_animo_1", 0));
-*/                      builder.setSelection(today);
-                niveles_animo(preferences.getInt("nivel_animo_1", 0),"1");
-            }
-       });
     }
-
-
-    public void niveles_animo (int nivel_animo,String dia_animo){
-
-        Bundle extras= new Bundle();
-        extras.putString("dia",dia_animo);
-        extras.putString("estado","");
-
-
-        switch(nivel_animo)
-        {
-            case 1:
-                extras.putString("estado","¡ Me siento TERRIBLE !");
-                Intent i1= new Intent(getContext(),TerribleActivity.class);
-                i1.putExtras(extras);
-                startActivity(i1);
-
-                break;
-
-            case 2:
-                extras.putString("estado","¡ Me siento MAL !");
-                Intent i2= new Intent(getContext(),MalActivity.class);
-                i2.putExtras(extras);
-                startActivity(i2);
-
-                break;
-
-            case 3:
-                extras.putString("estado","¡ Me siento BIEN !");
-                Intent i3= new Intent(getContext(),BienActivity.class);
-                i3.putExtras(extras);
-                startActivity(i3);
-
-                break;
-
-            case 4:
-                extras.putString("estado","¡ Me siento MUY BIEN !");
-                Intent i4= new Intent(getContext(),MuyBienActivity.class);
-                i4.putExtras(extras);
-                startActivity(i4);
-
-                break;
-
-            case 5:
-                extras.putString("estado","¡ Me siento GENIAL !");
-                Intent i5= new Intent(getContext(),GenialActivity.class);
-                i5.putExtras(extras);
-                startActivity(i5);
-
-                break;
-
-        }
-
-    }
-
 
 }
