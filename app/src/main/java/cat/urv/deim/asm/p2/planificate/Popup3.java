33908c7e19f0d9a0fea67b69cef2b8bf167b3f7b
maygd <maygd27@gmail.com>
2021-03-23 12:09:22 +0100

    Commit del 23 marzo (commint anterior) -> ERROR
    En este commit, FUNCIONAMIENTO ok CALEND+EST_ANIMO+REGISTRO

--- app/src/main/java/cat/urv/deim/asm/p2/planificate/Popup3.java
+++ app/src/main/java/cat/urv/deim/asm/p2/planificate/Popup3.java
@@ -1,6 +1,11 @@
 package cat.urv.deim.asm.p2.planificate;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +15,26 @@ public class Popup3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup3);
+
+
+
+        DisplayMetrics medidasVentana= new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(medidasVentana);
+
+        int ancho=medidasVentana.widthPixels;
+        int alto=medidasVentana.heightPixels;
+
+        getWindow().setLayout((int) (ancho*0.85),(int)(alto*0.5));
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor objEditor = preferences.edit();
+
+    }
+
+    public void onClick(View view) {
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+
     }
 }
